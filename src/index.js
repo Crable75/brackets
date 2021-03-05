2664f0386a312e29ad9164b739dbc6e124f37ccc
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2021-03-05 20:25:43 +0200

    feat: add resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+// function check(str, bracketsConfig) {
+  let strLength = str.length
+  let resFlag = true
+
+  while (resFlag && str.length > 0) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1],'')
+    }
+    if (strLength === str.length) {
+      resFlag = false
+    } else {
+      strLength = str.length
+    }
+  }
+  return str.length === 0
 }
+
+
